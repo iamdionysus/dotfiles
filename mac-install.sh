d17e4ad1f400f55ffa4467aa17b362f6bc7ee2b1
iamdionysus <iamdionysus@gmail.com>
2015-11-07 02:13:10 +0800

    Add mac install script

---
+++ mac-install.sh
@@ -0,0 +1,21 @@
+# Install homebrew
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Install common
+brew install ack \
+             curl wget
+             git \
+             redis
+
+# Install homebrew cask
+brew install caskroom/cask/brew-cask
+
+# Install apps
+brew cask install github-desktop
+brew cask install google-chrome
+
+#Set a blazingly fast keyboard repeat rate
+defaults write NSGlobalDomain KeyRepeat -int 0.02
+
+#Set a shorter Delay until key repeat
+defaults write NSGlobalDomain InitialKeyRepeat -int 12
